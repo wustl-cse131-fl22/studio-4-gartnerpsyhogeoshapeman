4349fc8a18bbbbc42517851996396393ec601a12
vipla <vipla@DESKTOP-P96TF56.dhcp.wustl.edu>
2022-10-13 12:45:48 -0500

    flag woop

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,63 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.3);
+		
+		StdDraw.setPenColor(0,80,50);
+		
+		StdDraw.filledCircle(0.17,0.35,0.1);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledCircle(0.2,0.35,0.1);
+		
+		StdDraw.setPenColor(0,80,50);
+		double[] tailX = {0.1,0.15,0.17};
+		double[] tailY = {0.25,0.22,0.28};
+		StdDraw.filledPolygon(tailX, tailY);
+		
+		StdDraw.filledCircle(0.3, 0.55, 0.25);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledCircle(0.15, 0.65, 0.25);
+		
+		StdDraw.setPenColor(0,80,50);
+		StdDraw.filledCircle(0.6, 0.65, 0.18);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledCircle(0.58, 0.8, 0.18);
+		StdDraw.filledCircle(0.45, 0.8, 0.15);
+		
+		StdDraw.setPenColor(0,80,50);
+		StdDraw.filledRectangle(0.82, 0.68, 0.05, 0.02);
+		StdDraw.filledRectangle(0.82, 0.62, 0.05, 0.02);
+		
+		StdDraw.setPenColor(184,0,255);
+		StdDraw.filledCircle(0.82, 0.68, 0.01);
+		
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledCircle(0.85,0.5,0.07);
+		double[] bodyX = {0.82,0.87,0.73,0.7};
+		double[] bodyY = {0.5,0.46,0.35,0.38};
+		
+		StdDraw.filledPolygon(bodyX,bodyY);
+		
+		StdDraw.setPenColor(0,255,255);
+		StdDraw.filledCircle(0.83, 0.53, 0.01);
+		StdDraw.filledCircle(0.87,0.5,0.01);
+		
+		
+		
+		
+		
+		
+		
+		
+		StdDraw.setPenColor(120,20,30);
+		StdDraw.filledRectangle(0.35, 0.65, 0.2, 0.12);
+		
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledRectangle(0.5, 0.9, 0.5, 0.1);
+		StdDraw.filledRectangle(0.5, 0.1, 0.5, 0.1);
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,69 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = 0;
+		double parameterTwo = 0;
+		double parameterThree = 0;
+		double parameterFour = 0;
+		double x1 = 0;
+		double y1 = 0;
+		double x2 = 0;
+		double y2 = 0;
+		double x3 = 0;
+		double y3 = 0;
+		
+		
+		if (shapeType.equals("triangle")) {
+			x1 = in.nextDouble();
+			y1 = in.nextDouble();
+			x2 = in.nextDouble();
+			y2 = in.nextDouble();
+			x3 = in.nextDouble();
+			y3 = in.nextDouble();
+		} else {
+			parameterOne = in.nextDouble();
+			parameterTwo = in.nextDouble();
+			parameterThree = in.nextDouble();
+			parameterFour = in.nextDouble();
+		}
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		if (shapeType.equals("ellipse")) {
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			if (isFilled)
+			{
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		
+		if (shapeType.equals("rectangle")) {
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			if (isFilled)
+			{
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		
+		if (shapeType.equals("triangle")) {
+			double[] xs = {x1, x2, x3};
+			double[] ys = {y1, y2, y3};
+			StdDraw.polygon(xs, ys);
+			if (isFilled)
+			{
+				StdDraw.filledPolygon(xs, ys);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
